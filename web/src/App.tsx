7682d79fbfe23e7e5e267ff02f011f8c0cc09786
reudismam <reudismam@gmail.com>
2020-06-04 19:46:07 -0300

    Starting creating page create-points.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Routes from './routes';
 
 function App() {
   let counter = 1;
@@ -12,9 +13,7 @@ function App() {
   
   return (
     <div>
-      <Header title="Hello World" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    <Routes />
     </div>
   );
 }
